22ba30d47f924eb30143f3618504ef68b351a23e
evgeniy1503 <evgeniy15031991@mail.ru>
2022-10-20 11:23:59 +0300

    fix all java file

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -7,32 +7,24 @@ public class MapSchema extends BaseSchema {
 
 
     public MapSchema() {
-
         addValid(x -> x instanceof Map<?, ?>);
-
     }
 
     public final void required() {
-
         setRequired(true);
-
     }
 
 
 
     public final void sizeof(int size) {
-
         Predicate<Map> sizeof = x -> x.size()  == size;
         addValid(sizeof);
-
     }
 
     public final MapSchema shape(Map<String, BaseSchema> map) {
-
         Predicate<Map> shape = x -> checkValid(x, map);
         addValid(shape);
         return this;
-
     }
 
     public final boolean checkValid(Map<String, String> data, Map<String, BaseSchema> map) {
--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -6,32 +6,25 @@ import java.util.function.Predicate;
 public class StringSchema extends BaseSchema {
 
     public StringSchema() {
-
         addValid(x -> x instanceof String);
-
     }
-    public final StringSchema required() {
 
+    public final StringSchema required() {
         Predicate<String> required = x -> x != null && !(x.isEmpty());
         addValid(required);
         setRequired(true);
         return this;
-
     }
 
     public final StringSchema contains(String str) {
-
         Predicate<String> contains = x -> x.contains(str);
         addValid(contains);
         return this;
-
     }
 
     public final void minLength(int number) {
-
         Predicate<String> minLength = x -> x.length() >= number;
         addValid(minLength);
-
     }
 
 }
